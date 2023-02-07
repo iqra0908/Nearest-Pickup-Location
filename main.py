ae3ca2d7eaa09cbdd02af0d422ad7eaf530d37ac
iqra0908 <44442059+iqra0908@users.noreply.github.com>
2023-02-07 23:20:13 +0000

    adding back service

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from fastapi import FastAPI
 import uvicorn
-##from Service import Service 
-##import json
+from Service import Service 
+import json
 
 app = FastAPI()
-##service = Service('directory.csv')
+service = Service('directory.csv')
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-##@app.get("/nearestStarbucks/{country_code}/{zip_code}")
-##async def getNearestStarbucks(country_code: str = '', zip_code: str = ''):
-##    return json.loads(service.getNearestStarbucks(country_code,zip_code))
+@app.get("/nearestStarbucks/{country_code}/{zip_code}")
+async def getNearestStarbucks(country_code: str = '', zip_code: str = ''):
+    return json.loads(service.getNearestStarbucks(country_code,zip_code))
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='0.0.0.0', port=8000)
