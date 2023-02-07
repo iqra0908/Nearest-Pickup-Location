e2fecbc723ab957b6f79e3586ed1e72b4d2edc7b
iqra0908 <44442059+iqra0908@users.noreply.github.com>
2023-02-07 22:32:53 +0000

    commenting out service

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+##from Service import Service 
+##import json
+
+app = FastAPI()
+##service = Service('directory.csv')
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+##@app.get("/nearestStarbucks/{country_code}/{zip_code}")
+##async def getNearestStarbucks(country_code: str = '', zip_code: str = ''):
+##    return json.loads(service.getNearestStarbucks(country_code,zip_code))
